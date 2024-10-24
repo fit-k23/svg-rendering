21b4a3408bf7d34def36697a661ce64449e7cc81
hanhjtrang256 <147674017+hanhtrang256@users.noreply.github.com>
2024-10-24 19:25:02 +0700

    Update Rectangle.h, nearly finish

--- src/element/shape/Rectangle.h
+++ src/element/shape/Rectangle.h
@@ -1,18 +1,85 @@
-#ifndef SVG_RENDERING_RECTANGLE_H
-#define SVG_RENDERING_RECTANGLE_H
+#ifndef SVG_RENDERING_Rect_H
+#define SVG_RENDERING_Rect_H
 
+#include "../Vector2D.h"
 #include "../Element.h"
+#include "../SVGColor.h"
 
-class Rectangle : public Element {
-public:
-	const ElementType type = ElementType::Rect;
+class Rect : public Element {
 private:
-	int x;
-	int y;
-	int width;
-	int height;
-	int rx;
-	int ry;
+	double width;
+	double height;
+	Vector2D<double> radii;
+public:
+	/*
+	* @cmt: Default constructor
+	*/
+	Rect();
+
+	/*
+	* @cmt: Parameterized constructor
+	* @param: position: vector2D position of Rect
+	* @param: width: width of Rect
+	* @param: height: height of Rect
+	* @param: radii: radii of Rect
+	* @param: fillColor: fill color of Rect
+	* @param: strokeColor: stroke color of Rect
+	* @param: strokeWidth: stroke width of Rect
+	*/
+	Rect(Vector2D<double> _position, const SVGColor& fillColor, const SVGColor& strokeColor, double strokeWidth, 
+			  double width, double height, Vector2D<double>radii);
+	
+	/*
+	* @cmt: get type of Rect 
+	* @return: ElementType::Rect
+	*/
+	ElementType getTypeName() override;
+
+	/*
+	* @cmt: draw Rect
+	*/
+	void draw() override;
+
+	/*
+	* @cmt: print data of Rect for debugging
+	*/
+	void dbg() override;
+
+	/*
+	* @cmt: set width of the Rect
+	* @param: _width: passed width 
+	*/
+	void setWidth(double _width);
+
+	/*
+	* @cmt: set height of the Rect
+	* @param: _height: passed height
+	*/
+	void setHeight(double _height);
+
+	/*
+	* @cmt: get width of the Rect
+	* @return: width of current Rect
+	*/
+	double getWidth();
+
+	/*
+	* @cmt: get height of the Rect
+	* @return: height of current Rect
+	*/
+	double getHeight();
+
+	/*
+	* @cmt: set radii of the Rect
+	* @param: radii: passed-by-reference radii
+	*/
+	void setRadii(const Vector2D<double>& radii);
+
+	/*
+	* @cmt: get radii of the Rect
+	* @return: radii of current Rect
+	*/
+	Vector2D<double> getRadii();
 };
 
-#endif //SVG_RENDERING_RECTANGLE_H
+#endif //SVG_RENDERING_Rect_H
