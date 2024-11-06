63c77f83aefa882fd1cf6ee35235712e10def7a8
NTT1906 <nttispay@gmail.com>
2024-11-06 22:27:36 +0700

    CHANGE: added a method to build with g++ and change the privacy of ViewBox

--- src/element/utils/ViewBox.cpp
+++ src/element/utils/ViewBox.cpp
@@ -1,10 +1,14 @@
 #include "ViewBox.h"
 
+ViewBox::ViewBox() : minX{0.0f}, minY{0.0f}, width{0.0f}, height{0.0f} {}
+
+ViewBox::ViewBox(float _minX, float _minY, float _width, float _height) : minX{_minX}, minY{_minY}, width{_width}, height{_height}  {}
+
 /** @brief Get the minimum X of ViewBox */
-float ViewBox::getMinX() const { return min_x; }
+float ViewBox::getMinX() const { return minX; }
 
 /** @brief Get the minimum Y of ViewBox */
-float ViewBox::getMinY() const { return min_y; }
+float ViewBox::getMinY() const { return minY; }
 
 /** @brief Get the width of ViewBox */
 float ViewBox::getWidth() const { return width; }
--- src/element/utils/ViewBox.h
+++ src/element/utils/ViewBox.h
@@ -2,11 +2,13 @@
 #define SVG_RENDERING_VIEW_BOX_H
 
 class ViewBox{
-	float min_x;
-	float min_y;
+	float minX;
+	float minY;
 	float width;
 	float height;
 public:
+	ViewBox();
+	ViewBox(float minX, float minY, float width, float height);
 	float getMinX() const;
 	float getMinY() const;
 	float getWidth() const;
