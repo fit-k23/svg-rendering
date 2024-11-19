c68bcf1454b38698dca12b3788416a95bec30b2f
NTT1906 <nttispay@gmail.com>
2024-11-19 23:59:30 +0700

    CHANGE: Minor works on file manager

--- src/api/FileManager.cpp
+++ src/api/FileManager.cpp
@@ -2,9 +2,6 @@
 
 std::vector<std::wstring> FileManager::filePaths;
 
-std::vector<std::wstring> &FileManager::getFilePaths() {
-	return filePaths;
-}
 bool FileManager::addFile(const std::wstring &filePath) {
 	if (filePath.empty() || !isFileExist(filePath)) return false;
 	for (auto &file : filePaths) {
@@ -17,16 +14,18 @@ bool FileManager::addFile(const std::wstring &filePath) {
 }
 
 bool FileManager::removeFile(const std::wstring &filePath) {
-	for (auto &file : filePaths) {
-		if (file == filePath) {
-			return true;
-		}
+	auto it = std::find(filePaths.begin(), filePaths.end(), filePath);
+	if (it != filePaths.end()) {
+		filePaths.erase(it);
+		return true;
 	}
 	return false;
 }
 
 bool FileManager::removeFile(size_t idx) {
-	return false;
+	if (idx == filePaths.size()) return false;
+	filePaths.erase(filePaths.begin() + idx); // NOLINT(*-narrowing-conversions)
+	return true;
 }
 
 bool FileManager::isFileExist(const std::wstring &filePath) {
@@ -40,4 +39,18 @@ bool FileManager::isFileExist(const std::wstring &filePath) {
 
 std::wstring FileManager::getBaseName(const std::wstring &filePath) {
 	return filePath.substr(filePath.find_last_of(L"/\\") + 1);
+}
+
+std::wstring FileManager::getFilePath(size_t idx) {
+	if (idx == filePaths.size()) return L"";
+	return filePaths.at(idx);
+}
+
+std::vector<std::wstring> FileManager::getFilePathList() {
+	std::vector<std::wstring> r;
+	r.reserve(filePaths.size());
+	for (auto &file : filePaths) {
+		r.push_back(FileManager::getBaseName(file));
+	}
+	return r;
 }
--- src/api/FileManager.h
+++ src/api/FileManager.h
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <vector>
 #include <fstream>
+#include <algorithm>
 
 class FileManager {
 	static std::vector<std::wstring> filePaths;
@@ -11,9 +12,10 @@ public:
 	static bool addFile(const std::wstring &filePath);
 	static bool removeFile(const std::wstring &filePath);
 	static bool removeFile(size_t idx);
-	static std::vector<std::wstring> &getFilePaths();
 	static bool isFileExist(const std::wstring &filePath);
-	static std::wstring getBaseName(const std::wstring &filePath);
+	static std::wstring getBaseName(const std::wstring &filePath) ;
+	static std::wstring getFilePath(size_t idx) ;
+	static std::vector<std::wstring> getFilePathList();
 };
 
 #endif //SVG_RENDERING_FILEMANAGER_H
